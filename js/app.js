9e38f839dfec81132c8740d97160791f8b35ee6a
AbdulAziz-210 <lexustoyota2002@gmail.com>
2024-03-13 23:27:51 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,108 @@
+
+
+
+
+// // 1-masala
+
+
+
+
+
+// // 2-masala
+
+// let a = 3
+// let b = newNumber(3);
+// let c = 3;
+
+// console.log(a == b); 
+// console.log(a === b); 
+// console.log(b === c); 
+
+
+// B variant : false, false, true
+
+
+
+// // 3-masala
+
+const numbers = [1, 2, 3]
+numbers[10] = 11;
+console.log(numbers);
+
+// C variant : [1, 2, 3, empty  x 7, 11]
+
+
+// // 4-masala
+
+// function bark () {
+//     console.log('woof!');
+// }
+// bark.animal = 'dog'
+
+// // B variant : You cannot add properties to a function this way,
+
+// Chunki function'ni animal degan method'i yo'q
+
+
+// // 5-masala
+
+// function sum(a, b) {
+//     return (a + b);
+// }
+// sum('1', '2')
+// console.log(sum);
+
+// // D variant : 3
+
+
+// // 6-masala
+
+// let number = 0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
+
+// // C varianti : 0, 2, 2
+
+// // 7-masala
+
+// function getPersonInfo (one, two, three) {
+//     console.log(one);
+//     console.log(two);
+//     console.log(three);
+// }
+
+// const person = "Lydia"
+// const age = 21
+
+// getPersonInfo `${person} is ${age} years old `;
+
+// // B varianti: [ '', ' is ', ' years old ' ] "Lydia" 21, `
+// // Vs code ga yozsam shu javob chiqdi, Masalaga uncha tushunmadim
+
+
+
+// // 8-masala
+
+// function checkAge(data){
+//     if (data === { age: 18 }) {
+//         console.log('you are an adult!');
+//     } else if (data == {age: 18}) {
+//         console.log('You are still an adult.');
+//     } else {
+//         console.log("Hmm.. You don't have an age I guess");
+//     }
+// }
+// checkAge({age : 18})
+
+
+// // C variant: Hmm.. You don't have an age I guess.
+
+
+
+
+
+// // ######   9-masalani tushunmadim    ####
+
+
+
